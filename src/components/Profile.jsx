f76354c926ec30c71ec8deafe2bc87de7146e8d6
Никита <nikit.suvorov@gmail.com>
2020-03-31 19:53:42 +0300

    добавлены css модули

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.content}>
             <div><img src="https://images.wallpaperscraft.com/image/tree_horizon_minimalism_128903_1280x720.jpg"
                       alt=""/>
             </div>
@@ -12,9 +13,9 @@ const Profile = () => {
             <div>My posts
                 <div>New posts</div>
             </div>
-            <div>
-                <div>post 1</div>
-                <div>post 2</div>
+            <div className={classes.posts}>
+                <div className={classes.item}>post 1</div>
+                <div className={classes.item}>post 2</div>
             </div>
         </div>
     );
