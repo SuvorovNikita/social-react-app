842d9add82055530c004fe4c2e259baae1d79278
SuvorovNikita <nikit.suvorv@gmail.com>
2020-03-31 15:35:07 +0300

    верстка grid

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import './App.css';
 
+
 const App = () => {
     return (
-        <div>
-            <div>One div</div>
-            <div className="App">
-                <ul>
-                    <li>css</li>
-                    <li>html</li>
-                    <li>js</li>
-                    <li>react</li>
-                </ul>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://pinger.kz/storage/files/8e2e294506441fc98ea30493f9803e22.png" alt=""/>
+            </header>
+            <nav className='nav'>
+                <div><a href=".#">Profile</a></div>
+                <div><a href=".#">Messages</a></div>
+                <div><a href=".#">News</a></div>
+                <div><a href=".#">Music</a></div>
+                <div><a href=".#">Settings</a></div>
+            </nav>
+            <div className='content'>
+                <div><img src="https://images.wallpaperscraft.com/image/tree_horizon_minimalism_128903_1280x720.jpg"
+                          alt=""/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>My posts
+                    <div>New posts</div>
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
             </div>
         </div>
-
     );
 }
+
 
 export default App;
