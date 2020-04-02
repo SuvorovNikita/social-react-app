71d828b4cc4d6dc27c7ce5f056365d785783be5c
SuvorovNikita <nikit.suvorv@gmail.com>
2020-04-02 16:41:40 +0300

    верстка dialog

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -2,10 +2,30 @@ import React from "react";
 import classes from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-    return ( <div>
-            Dialogs
+    return (<div className={classes.dialogs}>
+            <div className={classes.dialogsItem}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Nikita
+                </div>
+                <div className={classes.dialog}>
+                    Alisa
+                </div>
+                <div className={classes.dialog}>
+                    Zhenya
+                </div>
+                <div className={classes.dialog}>
+                    Sveta
+                </div>
+                <div className={classes.dialog}>
+                    Serega
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>kak dela?</div>
+                <div className={classes.message}>лялялял</div>
+            </div>
         </div>
-
     )
 };
 export default Dialogs;
